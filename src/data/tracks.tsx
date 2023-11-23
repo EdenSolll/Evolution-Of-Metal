ae41ea425ee3f7e5dac95fe0557494e9ec6f3a61
EdenSolll <erg9359g.rit.edu>
2023-11-23 03:35:17 -0500

    song lib update

--- src/data/tracks.tsx
+++ src/data/tracks.tsx
@@ -1,9 +1,63 @@
-import RainInBlood from './01.-Angel Of Death.flac'
+import AngelofDeath from './01.-Angel Of Death.flac'
+import PieceByPiece from './02.-Piece By Piece.flac'
+import Necrophobic from './03.-Necrophobic.flac'
+import AlterOfSacrifice from './04.-Altar Of Sacrifice.flac'
+import JesusSaves from './05.-Jesus Saves.flac'
+import CriminallyInsane from './06.-Criminally Insane.flac'
+import Reborn from './07.-Reborn.flac'
+import Epidemic from './08.-Epidemic.flac'
+import Postmortem from './09.-Postmortem.flac'
+import RainingBlood from './10.-Raining Blood.flac'
 
 export const tracks = [
   {
-    title: 'Angel Of Death',
-    src: RainInBlood,
+    title: 'Angel of Death',
+    src: AngelofDeath,
+    author: 'Slayer',
+  },
+  {
+    title: 'Piece by Piece',
+    src: PieceByPiece,
+    author: 'Slayer',
+  },
+  {
+    title: 'Necrophobic',
+    src: Necrophobic,
+    author: 'Slayer',
+  },
+  {
+    title: 'Alter of Sacrifice',
+    src: AlterOfSacrifice,
+    author: 'Slayer',
+  },
+  {
+    title: 'Jesus Saves',
+    src: JesusSaves,
+    author: 'Slayer',
+  },
+  {
+    title: 'Criminally Insane',
+    src: CriminallyInsane,
+    author: 'Slayer',
+  },
+  {
+    title: 'Reborn',
+    src: Reborn,
+    author: 'Slayer',
+  },
+  {
+    title: 'Epidemic',
+    src: Epidemic,
+    author: 'Slayer',
+  },
+  {
+    title: 'Postmortem',
+    src: Postmortem,
+    author: 'Slayer',
+  },
+  {
+    title: 'Raining Blood',
+    src: RainingBlood,
     author: 'Slayer',
   },
 ];
