44968a981eb302906e60afee8ab0a9a3b8a9fcb3
EdenSolll <erg9359g.rit.edu>
2023-12-14 16:20:55 -0500

    massive progress on map, go crush your cs final and come back to it.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -8,7 +8,7 @@ export default function Map() {
  const imageOverlay = useRef<L.ImageOverlay | null>(null);
  const mapContainer = useRef<L.Map | null>(null);
 
- useEffect(() => {
+useEffect(() => {
  if (mapContainer.current) {
  return;
  }
@@ -19,12 +19,11 @@ export default function Map() {
  maxZoom: 3,
  zoomSnap: 0.1,
  zoomDelta: 0.1,
- attributionControl: false
+ attributionControl: false,
  }).setView([200, -75], 0);
 
- const imageUrl = 'https://www.metal-archives.com/images/6/6/6/6/666668.jpg?0304'
- const bounds = [[-20, -20], [520, 520]] as L.LatLngBoundsExpression;
-
+ const imageUrl = 'https://www.metal-archives.com/images/6/6/6/6/666668.jpg?0304';
+ const bounds = [[0, 0], [540, 540]] as L.LatLngBoundsExpression;
 
  if (imageOverlay.current) {
  map.removeLayer(imageOverlay.current);
@@ -32,19 +31,64 @@ export default function Map() {
 
  imageOverlay.current = L.imageOverlay(imageUrl, bounds).addTo(map);
 
-
  lineLayer.current = L.layerGroup().addTo(map);
+
+ const labelLayer = L.layerGroup().addTo(map);
 
  Genres.forEach((genre) => {
  const polylineCoordinates: LatLngTuple[] = [
-  [genre.y_axis, 520],
-  [genre.y_axis, genre.year / 100],
+   [genre.y_axis, 540],
+   [genre.y_axis, ((genre.year)-1970)*10],
  ];
  L.polyline(polylineCoordinates, { color: 'red' }).addTo(map);
+
+ const marker = L.marker([genre.y_axis, (((genre.year-1970)*20))], {
+   icon: L.divIcon({
+     className: 'leaflet-marker-icon',
+     html: `<div>${genre.genre}</div>`,
+     iconSize: [200, 40]
+   })
+ }).addTo(map);
  });
 
+ const yearLayers = {};
+ for (let year = 1970; year <= 2024; year++) {
+ const marker = L.marker([0, ((year-1970)*10)], { icon: L.divIcon({ className: 'leaflet-marker-icon' }) }).bindTooltip(year.toString(), { permanent: year % 10 === 0, direction: 'bottom' });
+ if (!map.hasLayer(marker)) {
+  marker.addTo(map);
+ }
+ yearLayers[year] = marker;
+ }
+ const seen = [];
+ const replacer = (key, value) => {
+ if (value != null && typeof value === 'object') {
+  if (seen.indexOf(value) >= 0) {
+    return;
+  }
+  seen.push(value);
+ }
+ return value;
+};
+console.log(JSON.stringify(yearLayers, replacer));
+
+map.on('zoomend', () => {
+ const zoom = map.getZoom();
+ console.log('Zoom level:', zoom);
+ for (let year in yearLayers) {
+ if (zoom >= 2) {
+ console.log('Adding year marker for year:', year);
+ yearLayers[year].addTo(map);
+ } else {
+ if (year % 10 != 0){
+ console.log('Removing year marker for year:', year);
+ yearLayers[year].removeFrom(map);
+ }
+ }
+ }
+});
+
  mapContainer.current = map;
- }, []);
+}, []);
 
  return <div id="map" style={{ position: 'absolute', top: 0, left: 0, height: 'calc(100vh - 60px)', width: '100%' }}></div>;
 }
--- src/data/genres.tsx
+++ src/data/genres.tsx
@@ -1,43 +1,10 @@
 const Genres = [
   {
-  genre: "Alternative Metal",
-  year: 1985,
-  parents:[],
-  children:[],
-  y_axis: 215,
-  transition_genre: [],
-  },
-  {
-  genre: "Funk Metal",
-  year: 1995,
-  parents: ["Alternative Metal"],
-  children: ["Nu Metal"],
-  y_axis: 50,
-  transition_genre: ["Alternative Metal"],
-  },
-  {
-  genre: "Nu Metal",
-  year: 1993,
-  parents: ["Funk Metal"],
-  children: [],
-  y_axis: 200,
-  transition_genre: ["Funk Metal"],
-  },
-  {
-  genre: "Rap Metal",
-  year: 1987,
-  parents: ["Funk Metal"],
-  children: [],
-  y_axis: 150,
-  transition_genre: ["Funk Metal"],
-  },
-  {
   genre: "Black Metal",
   year: 1984,
   parents:["Alternative Metal"],
   children:[],
   y_axis: 100,
-  transition_genre: ["Alternative Metal"],
   },
 ];
 export default Genres;
